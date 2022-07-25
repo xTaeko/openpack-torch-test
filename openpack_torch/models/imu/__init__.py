40941e8889ccc562e36900b68b94f7505c29516f
Taeko <42623223+xTaeko@users.noreply.github.com>
2022-07-25 16:46:45 +0900

    Update __init__.py

--- openpack_torch/models/imu/__init__.py
+++ openpack_torch/models/imu/__init__.py
@@ -1,4 +1,5 @@
 from .deep_conv_lstm import DeepConvLSTM, DeepConvLSTMSelfAttn
 from .unet import UNet
+from .resnet import ResNet
 
-__all__ = ["DeepConvLSTM", "DeepConvLSTMSelfAttn", "UNet"]
+__all__ = ["DeepConvLSTM", "DeepConvLSTMSelfAttn", "UNet", "ResNet"]
